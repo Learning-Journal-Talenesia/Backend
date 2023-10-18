a7e0ef0a9e0016cfcdb3c0fb7b5e7a9ea562d0ae
Catfish-Emperor <muhild16@gmail.com>
2023-10-18 10:43:06 +0700

    Adding the crud functions

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createPost, getPosts} from '../controllers/post.js';
+import { createPost, getPosts, getPost, updatePost, deletePost} from '../controllers/post.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/',createPost)
+router.post('/',createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
